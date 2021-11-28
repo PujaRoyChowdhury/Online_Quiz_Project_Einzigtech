50326e0159e710b89df569c7a7edd45461fef90d
Puja Roy Chowdhury <roychowdhurypuja49@gmail.com>
2021-11-28 20:24:58 +0530

    exampage design

--- resources/views/exampage.blade.php
+++ resources/views/exampage.blade.php
@@ -14,12 +14,16 @@
                 @csrf
                 <div class="row">
                     @foreach ($qs as $q)
+                    <div>
                        <h4># {{$q->question}}</h4>
+                    </div>
+                    <div>
                        <input name="ans" value="a" type="radio">(A){{$q->a}}<br/>
                        <input name="ans" value="b" type="radio">(B) {{$q->b}}<br/>
                        <input name="ans" value="c" type="radio">(C) {{$q->c}}<br/>
                        <input name="ans" value="d" type="radio">(D) {{$q->d}}<br/>
                        <input name="dbans" value={{$q->ans}} style="visibility:hidden;" >
+                    </div>
                     @endforeach
                     <button type="submit" class="btn btn-primary ">Submit</button>
                 </div>
